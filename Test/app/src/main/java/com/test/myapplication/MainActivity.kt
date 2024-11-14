7167de82ed0f8a43bcae5ed73d4d5c933f6ef5cd
Bostero <benedicto_paco_o@hotmail.com>
2024-11-14 00:32:24 -0300

    test

--- Test/app/src/main/java/com/test/myapplication/MainActivity.kt
+++ Test/app/src/main/java/com/test/myapplication/MainActivity.kt
@@ -9,8 +9,9 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
