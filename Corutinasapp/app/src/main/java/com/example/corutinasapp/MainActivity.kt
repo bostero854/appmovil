3099403504ba2490134efd1175201703c880d376
Bostero <benedicto_paco_o@hotmail.com>
2024-12-03 18:39:03 -0300

    Inicio

---
+++ Corutinasapp/app/src/main/java/com/example/corutinasapp/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.corutinasapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.corutinasapp.ui.theme.CorutinasappTheme
+import androidx.compose.runtime.*
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CorutinasappTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Content(){
+    Column(
+        verticalArrangement =  Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ){
+        Text(text = "")
+        Button(onClick = {}) {
+            Text(text = "llamo a una api")
+
+        }
+
+    }
+}
+
+@Composable
+fun BotonColor(){
+    var color by remember{ mutableStateOf( false) }
+}
